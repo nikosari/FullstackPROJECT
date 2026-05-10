207ea8055edc12326b34c5a4df672bdffe22c281
nikosari <niko.sariola20@gmail.com>
2026-05-10 22:40:50 +0300

    final fixes

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -20,13 +20,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-   const initialState: State = { message: null, errors: {} };
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
- 
-  return <form action={formAction}>{/* ... */}</form>;
-}
-    <form>
+
+  return (
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -127,5 +126,6 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  ;
+  );
+}
 
